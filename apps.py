d530c6a5fcba444eb0c24286cd24dad242e48a66
15601071212 <15601071212@163.com>
2023-07-13 16:41:33 +0800

    Delete apps.py

--- apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-VERBOSE_APP_NAME = "设备资源管理"
-
-class LrmConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lrm'
-    verbose_name = VERBOSE_APP_NAME
